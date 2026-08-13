0cb372044bbb9713cb1d8128c020137589685b5a
Thaislaine Erica <thaiserica1304@gmail.com>
2026-08-13 20:17:14 -0300

    Mudei nome das variáveis para o mesmo nome das variáveis globais

--- classes/Busca.cs
+++ classes/Busca.cs
@@ -6,9 +6,9 @@ namespace sistema_cadastro_squad.classes
 {
     internal class Busca
     {
-        // 1. idades mudou para int[]
+        // 1. idade mudou para int[]
         // 2. totalCadastrados mudou para int
-        public static void BuscaPorNome(string[] nomes, int[] idades, double[] notas1, double[] notas2, int totalCadastrados)
+        public static void BuscaPorNome(string[] nome, int[] idade, double[] nota1, double[] nota2, int totalCadastrados)
         {
             Console.WriteLine("\n---Buscar aluno pelo nome---");
             Console.Write("Digite o nome do aluno que deseja buscar: ");
@@ -19,14 +19,14 @@ namespace sistema_cadastro_squad.classes
             // Percorre até o número total de alunos cadastrados
             for (int i = 0; i < totalCadastrados; i++)
             {
-                if (nomes[i] != null && nomes[i].Equals(nomeBusca, StringComparison.OrdinalIgnoreCase))
+                if (nome[i] != null && nome[i].Equals(nomeBusca, StringComparison.OrdinalIgnoreCase))
                 {
                     double media = (notas1[i] + notas2[i]) / 2.0;
 
-                    Console.WriteLine($"\nAluno encontrado: {nomes[i]}");
-                    Console.WriteLine($"Idade: {idades[i]}"); // Agora busca a idade do aluno no índice i
-                    Console.WriteLine($"Nota 1: {notas1[i]}");
-                    Console.WriteLine($"Nota 2: {notas2[i]}");
+                    Console.WriteLine($"\nAluno encontrado: {nome[i]}");
+                    Console.WriteLine($"Idade: {idade[i]}"); // Agora busca a idade do aluno no índice i
+                    Console.WriteLine($"Nota 1: {nota1[i]}");
+                    Console.WriteLine($"Nota 2: {nota2[i]}");
                     Console.WriteLine($"Média: {media:F1}");
 
                     alunoEncontrado = true;
