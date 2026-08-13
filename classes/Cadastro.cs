40565babff4d627cd629f4e628357e9b72c8fd96
Thais <thaiserica1304@gmail.com>
2026-08-13 19:42:12 -0300

    Feat: add lógica para verificar os alunos aprovados e formatação (quebra de linha) na listagem

--- classes/Cadastro.cs
+++ classes/Cadastro.cs
@@ -6,65 +6,45 @@ namespace sistema_cadastro_squad.classes
 {
     internal class Cadastro
     {
-        public void CadastrarAluno() 
-        { 
+        public void CadastrarAluno(string[] nome, int[] idade, int quantAlunos, double[] nota1, double[] nota2)
+        {
 
-            string[] nome = new string[10];
-            int[] idade = new int[10];
-            double[] nota1 = new double[10];
-            double[] nota2 = new double[10];
+        if (quantAlunos > 10)
+        {
+            Console.WriteLine("Você só pode cadastrar até 10 alunos.");
+        }
 
-            Console.WriteLine("Você deseja cadastrar algum aluno? (s/n)");
-            string resposta = Console.ReadLine();
+        else if (quantAlunos <= 0)
+        {
+            Console.WriteLine("Você deve cadastrar pelo menos 1 aluno.");
+        }
 
-            bool desejaCadastrar = resposta.ToLower() == "s";
-            if(desejaCadastrar != true)
+        else
+        {
 
+            for (int i = 0; i < quantAlunos; i++)
             {
-                Console.WriteLine(" ");
-                return;
-            }
+                Console.Write("Digite o nome do aluno: ");
+                nome[i] = Console.ReadLine();
 
-            Console.WriteLine("Quantos alunos você deseja cadastrar? ");
-            int quantAlunos = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Digite a idade do aluno: ");
+                idade[i] = Convert.ToInt32(Console.ReadLine());
 
-            if (quantAlunos > 10)
-            {
-                Console.WriteLine("Você só pode cadastrar até 10 alunos.");
-            }
+                Console.Write("Digite a primeira nota: ");
+                nota1[i] = Convert.ToDouble(Console.ReadLine());
 
-            else if (quantAlunos <= 0)
-            {
-                Console.WriteLine("Você deve cadastrar pelo menos 1 aluno.");
-            }
-
-            else
-            {
-
-                for (int i = 0; i < quantAlunos; i++)
-                {
-                    Console.Write("Digite o nome do aluno: ");
-                    nome[i] = Console.ReadLine();
-
-                    Console.Write("Digite a idade do aluno: ");
-                    idade[i] = Convert.ToInt32(Console.ReadLine());
-
-                    Console.Write("Digite a primeira nota: ");
-                    nota1[i] = Convert.ToDouble(Console.ReadLine());
-
-                    Console.Write("Digite a segunda nota: ");
-                    nota2[i] = Convert.ToDouble(Console.ReadLine());
-
-                    Console.WriteLine(" ");
-
-                }
+                Console.Write("Digite a segunda nota: ");
+                nota2[i] = Convert.ToDouble(Console.ReadLine());
 
                 Console.WriteLine(" ");
-                Console.WriteLine("~~Cadastro Concluído!~~");
-                Console.WriteLine(" ");
+
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("~~Cadastro Concluído!~~");
+            Console.WriteLine(" ");
 
             }
         }
     }
-
 }
